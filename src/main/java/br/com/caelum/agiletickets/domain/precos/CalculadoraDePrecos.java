eae7199bda61d4e7d2310a53511604b753c8c995
tca85 <thiago.alves.85@gmail.com>
2015-11-25 21:13:33 -0200

    refatoração do método Calcula

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -1,7 +1,5 @@
 package br.com.caelum.agiletickets.domain.precos;
-
 import java.math.BigDecimal;
-
 import br.com.caelum.agiletickets.models.Sessao;
 import br.com.caelum.agiletickets.models.TipoDeEspetaculo;
 
